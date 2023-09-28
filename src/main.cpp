8b2b7ad560795e645ad7743cfad1e70424686e21
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2023-09-28 11:53:52 +0200

    Evalready

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rubennijhuis <rubennijhuis@student.coda      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/13 16:38:00 by rubennijhui   #+#    #+#                 */
-/*   Updated: 2023/09/28 02:47:50 by daaf          ########   odam.nl         */
+/*   Updated: 2023/09/28 11:51:55 by dyeboa        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,29 @@
 
 using namespace std;
 
-void leaks(void){
-    system("leaks -q webserv_dev");
+void	Settings::freeSettings(std::vector<std::pair<int, Settings* > > &Config_Vector){
+	std::vector<std::pair<int, Settings* > >::iterator it;
+	std::vector<std::pair<std::string, Settings* > >::iterator it_location;
+	for (it = Config_Vector.begin(); it != Config_Vector.end(); it++){
+		std::vector<std::pair<std::string, Settings*> > locationsCopy = Config_Vector.begin()->second->getLocations();
+    	for (it_location = locationsCopy.begin(); it_location != locationsCopy.end(); it_location++) {
+			if (it_location->second != NULL)
+				delete it_location->second;
+    	}
+		delete it->second;
+		
+	}
 }
 
 int main(int argc, char* argv[])
 {
     std::vector<pair<int, Settings* > > Config_Vect;
-   	// atexit(leaks);
 	if (argc == 2){
 		ParseConfig config(argv[1]);
 		Config_Vect = config.ParseConfigFile();
        	// config.PrintVector(Config_Vect);
 		ServerControl   serverControl(Config_Vect);
+		Settings::freeSettings(Config_Vect);
 	}
     return 0;
 }
